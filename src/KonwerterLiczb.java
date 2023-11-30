253e01f9016e35b877b4c611fada56457d184aea
Dawid-Stankowski <dawid441@interia.pl>
2023-11-30 12:18:00 +0100

    1.0

--- src/KonwerterLiczb.java
+++ src/KonwerterLiczb.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class KonwerterLiczb {
+public class KonwerterLiczb {
+    public static void main(String[] args) {
+        String tekst = "3532467";
+
+        // Konwersja na int
+        int liczbaInt = Integer.parseInt(tekst);
+        System.out.println("Liczba int: " + liczbaInt);
+
+        // Konwersja na long
+        long liczbaLong = Long.parseLong(tekst);
+        System.out.println("Liczba long: " + liczbaLong);
+
+        // Konwersja na float
+        float liczbaFloat = Float.parseFloat(tekst);
+        System.out.println("Liczba float: " + liczbaFloat);
+
+        // Konwersja na double
+        double liczbaDouble = Double.parseDouble(tekst);
+        System.out.println("Liczba double: " + liczbaDouble);
+    }
 }
